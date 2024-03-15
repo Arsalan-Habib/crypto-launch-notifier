a7e7624152ef095169c695f95505d5b4e30cf95c
web3dev00 <development@dechains.com>
2024-03-15 14:38:54 +0500

    refactor: make port 3000 fallback

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const RPC = "https://polygon-mumbai-bor.publicnode.com";
 export const BOT_TOKEN = `${process.env.BOT_TOKEN}` || "";
 export const OPENAI_API_KEY = `${process.env.OPENAI_API_KEY}` || "";
 export const ETHERSCAN_API_KEY = `${process.env.ETHERSCAN_API_KEY}` || "";
-export const PORT = 3000;
+export const PORT = process.env.PORT || 3000;
 export const MONGO_URI = `${process.env.MONGO_URI}` || "";
 
 export const ignoredLinks = ["eips.ethereum.org", "consensys", "solidity.readthedocs"];
